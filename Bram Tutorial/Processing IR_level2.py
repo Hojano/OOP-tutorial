84deb73834fbc5b1ac5bf5340c5a16a76559a740
Bram Kappé <b.t.kappe@uu.nl>
2025-06-11 14:54:27 +0200

    Added tutorial on classes and dictionaries

---
+++ Bram Tutorial/Processing IR_level2.py
@@ -0,0 +1,117 @@
+import numpy as np
+import collections
+import os
+import pandas as pd
+import openpyxl
+import matplotlib.pyplot as plt
+import scipy as scp
+import tkinter as tk
+import jcamp as j
+from tkinter import filedialog
+
+def to_float(x):
+    try:
+        return float(x)
+    except:
+        return x
+
+
+def load_data(FolderName):
+    filelist = []
+    #Find all .dx files in the folder specified and add them to the list of files
+    for root, dirs, files in os.walk(FolderName, topdown=True):
+            for name in files:
+                if '.dx' in name:
+                    filelist.append(os.path.join(root, name))
+
+    #Load .dx files into dataframe
+    firsttime = True
+
+    for files in filelist:
+        if firsttime:
+            #For the first time, I want to read the wavenumbers and recalculate them since their is/was a bug in that.
+            FirstSpectrum = j.jcamp_readfile(files)
+            WavenumbersTemp = FirstSpectrum['x']
+            AmountOfWavenumbers = len(WavenumbersTemp)
+            HighestWaveNumber = WavenumbersTemp[0]
+            LowestWavenumber = WavenumbersTemp[-1]
+            CorrectWavenumbers = np.linspace(HighestWaveNumber, LowestWavenumber, num=AmountOfWavenumbers)
+
+            #defining a dataframe with those wavenumbers to then add the spectra in later
+            DF = pd.DataFrame({'Wavenumber': CorrectWavenumbers})
+            firsttime = False
+
+        #reading .dx file
+        try:
+            CurrentSpectrum = j.jcamp_readfile(files)
+        except:
+            print(str(CurrentNumber) + ' failed!!')
+
+        # adding Y values of spectrum to DF
+        DF = pd.concat([DF, pd.Series(CurrentSpectrum['y'])], axis=1)
+
+        # some code to print the progress
+        FilesKort = files.replace('.dx', '')
+        CurrentNumber = FilesKort[-4:]
+        if CurrentNumber[-2:] == '00':
+            print(CurrentNumber)
+
+    #making the DF the way I want it- with the wavenumbers as columns and the spectra being the rows.
+    DF = DF.set_index(('Wavenumber'))
+    DF = DF.transpose()
+    DF.columns = DF.columns.map(to_float)
+    DF = DF.reset_index(drop=True)
+    return DF
+
+def plot_allspectra(DF):
+    # Plotting all spectra
+    AllSpectraFig = plt.figure()
+    AllSpectraFigAx = AllSpectraFig.add_subplot()
+    for i in DF.index:
+        AllSpectraFigAx.plot(DF.columns, DF.iloc[i, :])
+
+def integrate_and_plot_CO2(DF):
+    # Integrating CO2 peak
+    # First finding some parameters for cutting the range
+    Wavenumbers = DF.columns
+    LowWaveNumberIndex = min(range(len(Wavenumbers)), key=lambda i: abs(Wavenumbers[i] - 2200))
+    HighWaveNumberIndex = min(range(len(Wavenumbers)), key=lambda i: abs(Wavenumbers[i] - 2400))
+
+    integrated_list = []
+    # then iterating over all spectra and integrating them
+    for i in DF.index:
+        # cut data to relevant area
+        SpectrumToIntegrate = DF.iloc[i, HighWaveNumberIndex:LowWaveNumberIndex]
+        CO2_area = scp.integrate.trapezoid(y=SpectrumToIntegrate, x=SpectrumToIntegrate.index)
+        # saving negative since the data is saved from high to low wavenumbers which makes the integration negative.
+        integrated_list.append(-CO2_area)
+
+    # Plot CO2 band over time
+    CO2Fig = plt.figure()
+    CO2FigAx = CO2Fig.add_subplot()
+    CO2FigAx.plot(integrated_list)
+
+
+FolderName = 'Raw Data/20230913_BK053_A_RERUN_correctmethod'
+DF_A = load_data(FolderName)
+plot_allspectra(DF_A)
+integrate_and_plot_CO2(DF_A)
+
+
+plt.show()
+
+
+
+
+
+
+
+
+
+#Delete at end!
+"""
+root = tk.Tk()
+root.withdraw()
+Folders = filedialog.askdirectory(parent=root, title='Choose f')
+print(Folders)
+"""
---
+++ Bram Tutorial/Processing IR_level3.py
@@ -0,0 +1,118 @@
+import numpy as np
+import collections
+import os
+import pandas as pd
+import openpyxl
+import matplotlib.pyplot as plt
+import scipy as scp
+import tkinter as tk
+import jcamp as j
+from tkinter import filedialog
+
+def to_float(x):
+    try:
+        return float(x)
+    except:
+        return x
+
+
+class IR_dataset:
+    def __init__(self,FolderName):
+        self.filelist = []
+        # Find all .dx files in the folder specified and add them to the list of files
+        for root, dirs, files in os.walk(FolderName, topdown=True):
+            for name in files:
+                if '.dx' in name:
+                    self.filelist.append(os.path.join(root, name))
+
+        # Load .dx files into dataframe
+        self.firsttime = True
+
+        for files in self.filelist:
+            if self.firsttime:
+                # For the first time, I want to read the wavenumbers and recalculate them since their is/was a bug in that.
+                self.FirstSpectrum = j.jcamp_readfile(files)
+                self.WavenumbersTemp = self.FirstSpectrum['x']
+                self.AmountOfWavenumbers = len(self.WavenumbersTemp)
+                self.HighestWaveNumber = self.WavenumbersTemp[0]
+                self.LowestWavenumber = self.WavenumbersTemp[-1]
+                self.CorrectWavenumbers = np.linspace(self.HighestWaveNumber, self.LowestWavenumber, num=self.AmountOfWavenumbers)
+
+                # defining a dataframe with those wavenumbers to then add the spectra in later
+                self.DF = pd.DataFrame({'Wavenumber': self.CorrectWavenumbers})
+                self.firsttime = False
+
+            # reading .dx file
+            try:
+                self.CurrentSpectrum = j.jcamp_readfile(files)
+            except:
+                print(str(self.CurrentNumber) + ' failed!!')
+
+            # adding Y values of spectrum to DF
+            self.DF = pd.concat([self.DF, pd.Series(self.CurrentSpectrum['y'])], axis=1)
+
+            # some code to print the progress
+            self.FilesKort = files.replace('.dx', '')
+            self.CurrentNumber = self.FilesKort[-4:]
+            if self.CurrentNumber[-2:] == '00':
+                print(self.CurrentNumber)
+
+        # making the DF the way I want it- with the wavenumbers as columns and the spectra being the rows.
+        self.DF = self.DF.set_index(('Wavenumber'))
+        self.DF = self.DF.transpose()
+        self.DF.columns = self.DF.columns.map(to_float)
+        self.DF = self.DF.reset_index(drop=True)
+
+
+    def plot_allspectra(self):
+        # Plotting all spectra
+        self.AllSpectraFig = plt.figure()
+        self.AllSpectraFigAx = self.AllSpectraFig.add_subplot()
+        for i in self.DF.index:
+            self.AllSpectraFigAx.plot(self.DF.columns, self.DF.iloc[i, :])
+
+    def integrate_and_plot_CO2(self):
+        # Integrating CO2 peak
+        # First finding some parameters for cutting the range
+        self.Wavenumbers = self.DF.columns
+        self.LowWaveNumberIndex = min(range(len(self.Wavenumbers)), key=lambda i: abs(self.Wavenumbers[i] - 2200))
+        self.HighWaveNumberIndex = min(range(len(self.Wavenumbers)), key=lambda i: abs(self.Wavenumbers[i] - 2400))
+
+        self.integrated_list = []
+        # then iterating over all spectra and integrating them
+        for i in self.DF.index:
+            # cut data to relevant area
+            self.SpectrumToIntegrate = self.DF.iloc[i, self.HighWaveNumberIndex:self.LowWaveNumberIndex]
+            self.CO2_area = scp.integrate.trapezoid(y=self.SpectrumToIntegrate, x=self.SpectrumToIntegrate.index)
+            # saving negative since the data is saved from high to low wavenumbers which makes the integration negative.
+            self.integrated_list.append(-self.CO2_area)
+
+        # Plot CO2 band over time
+        self.CO2Fig = plt.figure()
+        self.CO2FigAx = self.CO2Fig.add_subplot()
+        self.CO2FigAx.plot(self.integrated_list)
+
+
+FolderName = 'Raw Data/20230913_BK053_A_RERUN_correctmethod'
+A = IR_dataset(FolderName)
+A.plot_allspectra()
+A.integrate_and_plot_CO2()
+
+
+plt.show()
+
+
+
+
+
+
+
+
+
+#Delete at end!
+"""
+root = tk.Tk()
+root.withdraw()
+Folders = filedialog.askdirectory(parent=root, title='Choose f')
+print(Folders)
+"""
